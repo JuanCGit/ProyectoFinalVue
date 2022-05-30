b2c27cb078e3f7bb582940e32881ed2079bcefda
LucasEG1 <legyori@gmail.com>
2022-05-30 20:35:49 +0200

    About: miembros, descrip. de myGym, tecnologías.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,117 @@
 <template>
-  <div class="containerAbout">
-    <h1 class="mainColor">This is the about page</h1>
+  <div class="container about">
+    <div class="miembros">
+      <h1 class="mb-4 display-4">Nosotros</h1>
+
+      <div @click="abrirLink('https://github.com/JuanCGit')" class="row row-cols-1 row-cols-md-2 cards">
+        <div class="col-xxl-2 d-flex flex-column align-items-center justify-content-center"><img src=""/></div>
+        <div class="col-xxl-10 d-flex flex-column justify-content-center">
+            <h3>Juan Casanova</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur qui fugiat ullam! Perferendis accusantium, aut iste odit nostrum numquam minima error ab non! Omnis, voluptate aperiam ut nulla sed unde!</p>
+        </div>
+      </div>
+
+      <div @click="abrirLink('https://github.com/LucasEG1')" class="row row-cols-1 row-cols-md-2 cards">
+        <div class="col-xxl-2 d-flex flex-column align-items-center justify-content-center"><img src=""/></div>
+        <div class="col-xxl-10 d-flex flex-column justify-content-center">
+            <h3>Lucas Gyori</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+      </div>
+
+      <div @click="abrirLink('https://github.com/')" class="row row-cols-1 row-cols-md-2 cards">
+        <div class="col-xxl-2 d-flex flex-column align-items-center justify-content-center"><img src=""/></div>
+        <div class="col-xxl-10 d-flex flex-column justify-content-center">
+            <h3>Rubén Pérez Castelló</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas sequi minus minima dolore eaque quibusdam! Nisi placeat accusamus omnis ea asperiores sunt similique, vero in tenetur sed quos quis dolorem!</p>
+        </div>
+      </div>
+
+      <div @click="abrirLink('https://github.com/')" class="row row-cols-1 row-cols-md-2 cards">
+        <div class="col-xxl-2 d-flex flex-column align-items-center justify-content-center"><img src=""/></div>
+        <div class="col-xxl-10 d-flex flex-column justify-content-center">
+            <h3>Luis Perez Hurtado</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae repellendus tempore alias? Minima ullam omnis repellat voluptates perspiciatis explicabo, repellendus unde non laudantium, culpa, illum odio modi ut atque et!</p>
+        </div>
+      </div>
+    </div>
+    <!-- N U E S T R O  P R O Y E C T O -->
+    <div class="proyecto">
+      <h1 class="mb-4 display-4">Nuestro proyecto</h1>
+
+      <div class="row row-cols-1 row-cols-md-2 cards">
+        <div class="col-xxl-2 d-flex flex-column align-items-center justify-content-center"><img src="@/assets/myGym.png"/></div>
+        <div class="col-xxl-10 d-flex flex-column justify-content-center">
+            <h2 class="mainColor">My Gym</h2>
+            <p>My Gym es un gimnasio que despertará tu pasión por el ejercicio, el cuidado propio y la salud física.</p>
+            <p>Inscríbete ya y comienza a disfrutar de tus ventajas, como clases de entrenamiento personal con profesionales licenciados en
+              ciencias del deporte y la salud, grandes instalaciones con variedad de material y un clima espectacular que te mantendrá
+              motivado y así hacer de ti una persona más sana.
+            </p>
+        </div>
+      </div>
+    </div>
+    <div class="tecnologias">
+      <h1 class="mb-4 display-4">Tecnologías utilizadas</h1>
+      
+      <div  @click="abrirLink('https://vuejs.org/')" class="row row-cols-1 row-cols-md-2 cards">
+        <div class="col-xxl-2 d-flex flex-column align-items-center justify-content-center"><img src="@/assets/VueLogo.png"/></div>
+        <div class="col-xxl-10 d-flex flex-column justify-content-center">
+            <h2 style="color: #42b883">Vue.js</h2>
+            <p>Vue.js es un framework de JavaScript de código abierto para la construcción de interfaces de usuario y aplicaciones de una sola página. Fue creado por Evan You, y es mantenido por él y por el resto de los miembros activos del equipo central que provienen de diversas empresas como Netlify y Netguru.</p>
+        </div>
+      </div>
+      <div  @click="abrirLink('https://nodejs.org/es/')" class="row row-cols-1 row-cols-md-2 cards">
+        <div class="col-xxl-2 d-flex flex-column align-items-center justify-content-center"><img src="@/assets/NodeJSLogo.png"/></div>
+        <div class="col-xxl-10 d-flex flex-column justify-content-center">
+            <h2 style="color: #8ac400">Node.js</h2>
+            <p>Node.js es un entorno en tiempo de ejecución multiplataforma, de código abierto, para la capa del servidor basado en el lenguaje de programación JavaScript, asíncrono, con E/S de datos en una arquitectura orientada a eventos y basado en el motor V8 de Google.</p>
+        </div>
+      </div>
+      <div @click="abrirLink('https://getbootstrap.com/')" class="row row-cols-1 row-cols-md-2 cards">
+        <div class="col-xxl-2 d-flex flex-column align-items-center justify-content-center"><img src="@/assets/BootstrapLogo.png"/></div>
+        <div class="col-xxl-10 d-flex flex-column justify-content-center">
+            <h2 style="color: #7952b3">Bootstrap</h2>
+            <p>Bootstrap 5 es la versión más reciente de Bootstrap, el framework más popular para el desarrollo front-end responsivo.</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'About-Vue'
+  name: 'About-Vue',
+  methods: { 
+      abrirLink(url) {
+          window.open(url, '_blank');
+      }
+  }
 }
 </script>
 
-<style scoped>
-.containerAbout {
-  margin: 100px 0 100px 0;
+<style scoped lang="scss">
+.about {
+  padding-top: 50px;
+  padding-bottom: 50px;
+
+  p {
+  font-size: larger;
+  }
+  img {
+    width: 100%;
+  }
+}
+.tecnologias, .proyecto {
+  h2 {
+    font-weight: bold;
+  }
+}
+.cards {
+    padding: 25px;
+    box-shadow: 0 30px 80px rgba(0,0,0,0), 0 20px 70px rgba(0,0,0,0.12);
+    border-radius: 15px;
+    margin-bottom: 50px;
+    cursor: pointer;
 }
 </style>
